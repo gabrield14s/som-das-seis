a0057641f7ab4e23a389f2c4bbf519b1eaddf2de
gabrield14s <gabrieldiasda321@gmail.com>
2025-01-04 00:33:54 -0300

    start project (talvez eu apague todo o css que eu fiz e talvez faça a estilização em css do zero do chamber.svg)

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Atributes from './Pages/Atributes';
+import './App.css';
+
+function App() {
+  return (
+      <>
+        <Atributes></Atributes>
+      </>
+  );
+}
+
+export default App;
